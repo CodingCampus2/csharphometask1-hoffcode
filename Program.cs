f5431424cd202576870b90cee4cb53572bd584fc
rhofman <hoffroma24@gmail.com>
2020-03-25 10:29:13 +0200

    task complete

--- Program.cs
+++ Program.cs
@@ -9,11 +9,19 @@ namespace HomeworkTemplate
         {
             Func<Task1, string> TaskSolver = task =>
             {
-                // Your solution goes here
-                // You can get all needed inputs from task.[Property]
-                // Good luck!
+                int population = int.Parse(task.City.Population);
+                float sickPercentage = float.Parse(task.City.SickPercentage);
+                float killProbability = float.Parse(task.Virus.KillProbability);
 
-                return "My answer";
+                float sickPopulation = (float)population * sickPercentage;
+                int sickPopulationInt = (int)Math.Truncate(sickPopulation);
+
+                float diedPopulation = killProbability * sickPopulation;
+                int diedPopulationInt = (int)Math.Truncate(diedPopulation);
+
+                string result = $"There are {sickPopulationInt} people sick with {task.Virus.Name} in the city of {task.City.Name}, {diedPopulationInt} of which died";
+
+                return result;
             };
 
             Task1.CheckSolver(TaskSolver);
